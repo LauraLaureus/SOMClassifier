fdebfb58679e125fc3650d3ba5c347db76251513
Laura <l4depende@gmail.com>
2015-12-14 08:21:59 +0000

    GetBMU structure

--- Main.m
+++ Main.m
@@ -1,1 +1,17 @@
-readData()
+function Main(episodes)
+
+%Read Data from file
+Data = readData();
+
+%Neuron matrix 10x16, weights 4;
+N = rand(10,16,4);
+
+for e=1:episodes
+    for i=1:size(Data,1)
+        [BMU,x,y] = getBMU(N,Data(i));
+        N = updateWeights(BMU,x,y,N);
+    end
+end
+
+%Abría que plasmar las salidas en un plot.
+end
---
+++ getBMU.m
@@ -0,0 +1,20 @@
+function [BMU,x,y] = getBMU(Neurons,example)
+
+BMU = [];
+x = 0;
+y = 0;
+error_for_BMU = Inf;
+
+for i=1:size(Neurons,1)
+    for j=1:size(Neurons,2)
+        error = 0; %computeError(Neurons(i,j,:),example);
+        if error < error_for_BMU
+            error_for_BMU = error;
+            x=i;
+            y=j;
+            BMU = Neurons(i,j,:);
+        end
+    end
+end
+
+end
