a0ae4621b84f9356795fd94d5a7af9fb457374dd
Laura <l4depende@gmail.com>
2015-12-21 20:58:52 +0000

    Update view

--- view.m
+++ view.m
@@ -2,9 +2,19 @@ function view(Data,N)
 
 figure;
 hold on;
-for example=1:50
+for example=1:50 %red
     [~,x,y]= getBMU(N,Data(example));
     plot(x,y,'or');
+end
+
+for example=51:100 %blue
+    [~,x,y]= getBMU(N,Data(example));
+    plot(x,y,'ob');
+end
+
+for example=101:150 %green
+    [~,x,y]= getBMU(N,Data(example));
+    plot(x,y,'og');
 end
 
 axis([0 10 0 16]);
