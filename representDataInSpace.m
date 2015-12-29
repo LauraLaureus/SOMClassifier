8b0322b79c74a4d876ce83de7294447bf51d325f
LauraLaureus <l4depende@gmail.com>
2015-12-29 20:13:30 +0000

    Update

--- representDataInSpace.m
+++ representDataInSpace.m
@@ -1,19 +1,20 @@
 function representDataInSpace(Data,SuperposedData)
 
-X = Data(:,1);
-Y = Data(:,2);
-Z = Data(:,3);
-
 figure;
+X = Data(:,2);
+Y = Data(:,3);
+Z = Data(:,4);
+
+cameratoolbar;
 plot3(X,Y,Z,'o');
 hold on 
-title('Data in space');
+title('Data in space 3');
 grid on;
 
-X2 = reshape(SuperposedData(:,:,1),[],1);
-Y2 = reshape(SuperposedData(:,:,2),[],1);
-Z2 = reshape(SuperposedData(:,:,3),[],1);
+X2 = reshape(SuperposedData(:,:,2),[],1);
+Y2 = reshape(SuperposedData(:,:,3),[],1);
+Z2 = reshape(SuperposedData(:,:,4),[],1);
 
-plot3(X2,Y2,Z2,'xr');
+plot3(X2,Y2,Z2,'dr');
 hold off;
 end
