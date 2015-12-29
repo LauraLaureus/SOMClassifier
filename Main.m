6a92cf7aab665551b617bbf956e4dbc23d61cdc4
Laura <l4depende@gmail.com>
2015-12-29 10:07:30 +0000

    Update

--- Main.m
+++ Main.m
@@ -3,12 +3,12 @@ function Main(episodes)
 %Read Data from file
 Data = readData();
 
-%Neuron matrix 10x16, weights 4;
-N = rand(10,16,4);
+%Neuron matrix 3x3, weights 4;
+N = rand(3,3,4);
 %sigma = 2.5;
 
 for e=1:episodes
-    sigma = sqrt(10*16);
+    sigma = 3;
     for i=1:size(Data,1)
         [BMU,x,y,Error] = getBMU(N,Data(i,:));
         [N,sigma] = updateWeights(BMU,x,y,N,Error,sigma,i*e,0.1);
--- updateWeights.m
+++ updateWeights.m
@@ -3,12 +3,16 @@ function [Neurons,sigma] = updateWeights(BMU,x,y,Neurons,Error,sigma,iteration,a
     tau = 8000/log(sigma); %sacado del ejemplo de Kohonen
     sigma = sigma*exp(-iteration/tau);
     
+    count = 0;
     for i=1:size(Neurons,1)
         for j=1:size(Neurons,2)
             neighbourhood_value = computeNeighbourhood(x,y,i,j,sigma);
             for k=1:size(Neurons,3)
                 Neurons(i,j,k) = Neurons(i,j,k) + neighbourhood_value*alpha*Error(i,j,k);
+                count = count + neighbourhood_value*alpha*Error(i,j,k);
             end
         end
     end
+    
+    disp(count / 36);
 end
